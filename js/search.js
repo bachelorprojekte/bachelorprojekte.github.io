9507466c1b1cd1587fac689d5610ef435f2dd27d
Marcus Ding <marcusding@me.com>
2018-03-06 20:18:31 +0100

    Improve search design

--- js/search.js
+++ js/search.js
@@ -7,7 +7,7 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h2>' + item.title + '</h2></a>';
+        appendString += '<li><span class="post-meta">' + item.date + '</span><a href="' + item.url + '"><h2>' + item.title + '</h2></a>';
         appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
       }
 
